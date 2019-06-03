f163fb0e8de9f5f020d9bdf487f6b1955ed07e41
Fabien LEFEBVRE <contact@d1ceward.com>
2019-06-03 09:56:39 +0200

    Added current version verif on update script

--- update.sh
+++ update.sh
@@ -1,5 +1,13 @@
+# Get current release name
+CURRENT_RELEASE=$(git tag | tail -1)
+
 # Get lastest release name
 RELEASE=$(curl --silent "https://github.com/grafana/grafana/releases/latest" | sed 's#.*tag/\(.*\)\".*#\1#' | cut -f2 -d 'v')
+
+# Exit script if already up to date
+if [ "v${RELEASE}" = $CURRENT_RELEASE ]; then
+  exit 0
+fi
 
 # Replace "from" line in dockerfile with the new release
 sed -i "s#FROM.*#FROM grafana/grafana:${RELEASE}#" Dockerfile
